c9f07beb79ac8ca2e608d7c64c890e9eb9a49d0d
aangelo96 <aangelo@student.unimelb.edu.au>
2020-04-10 14:59:22 +1000

    Updating orderRoutes

--- konveksi-server/src/middlewares/requireAuth.js
+++ konveksi-server/src/middlewares/requireAuth.js
@@ -21,6 +21,6 @@ module.exports = (req, res, next) => {
     const user = await User.findById(userId);
     req.user = user;
     next();
-    
+
   });
 };
--- konveksi-server/src/routes/orderRoutes.js
+++ konveksi-server/src/routes/orderRoutes.js
@@ -1,8 +1,10 @@
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const mongoose = require('mongoose');
 
 const requireAuth = require('../middlewares/requireAuth');
 
+const User = mongoose.model('User');
 const OrderParam = mongoose.model('OrderParam');
 const Track = mongoose.model('Track');
 const Bid = mongoose.model('Bid');
@@ -17,7 +19,7 @@ router.post('/order', async (req,res) => {
   const { color , secColor, option, amount, budget, imgs } = req.body;
 
   try {
-    const lastUpdate = Date.now();
+    const lastUpdate = Date(Date.now());
 
     const track = new Track({ userId:req.user._id, status:'On Bidding', lastUpdate:lastUpdate, nextDeadline:'-', img:imgs[0] });
     await track.save();
@@ -27,7 +29,7 @@ router.post('/order', async (req,res) => {
       const orderParam = new OrderParam({ trackId:track._id, color:color, secColor:secColor, option:option, amount:amount, budget:budget, imgs:imgs});
       await orderParam.save();
 
-      res.send(orderParam);
+      res.send(track);
 
     } catch (err) {
 
@@ -47,21 +49,37 @@ router.post('/order', async (req,res) => {
 /*FETCHING A LIST OF ORDERS*/
 router.post('/fetchlist', async (req,res) => {
 
-  if (!req.user) {
-    return res.status(422).send({ error: 'Must be logged in'});
-  }
+  const {token} = req.body;
+  console.log(token);
 
-  try {
+  jwt.verify(token, 'my_secret_key', async (err, payload) => {
+    if (err) {
+      return res.status(401).send({ error: 'You must be logged in.' })
+    }
 
-    const orders = await Track.find({userId:req.user._id});
-    res.send(orders);
+    console.log("aaa");
+    const { userId } = payload;
 
+    const user = await User.findById(userId);
 
-  } catch (err) {
+    console.log("abc");
+    if (!user) {
+      return res.status(422).send({ error: 'Must be logged in'});
+    }
 
-    console.log(err);
+    try {
 
-  }
+      const orders = await Track.find({userId:user._id});
+      res.send(orders);
+
+    } catch (err) {
+
+      console.log(err);
+
+    }
+
+  });
+
 
 });
 
